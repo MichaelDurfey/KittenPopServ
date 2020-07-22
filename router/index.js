13929061640900df1b28417e1bacc61ada07ae5f
Michael Durfey <mdurfey@me.com>
2020-07-21 22:45:54 -0700

    george clooney

--- router/index.js
+++ router/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const path = require("path");
 const redis = require("redis");
-
+const client = redis.createClient(process.env.REDIS_URL);
 const router = express.Router();
 
+router.post("/leader", (req, res) => {
+  const newLeader = req.body.leader;
+  const score = req.body.score;
+  client.set("leaders", { sieun: 200 });
+  client.get("leaders", (err, reply) => {
+    res.send(reply);
+  });
+});
+
 router.get("/leaders", (req, res) => {
-  res.send("here are the leaders!!!!");
+  client.get("leaders", (err, reply) => {
+    if (err) {
+      res.send("Sorry, something went wrong: " + err);
+      return;
+    }
+    res.send({ leaders: reply });
+  });
+});
+
+client.on("error", function (error) {
+  console.error(error);
 });
 
 module.exports = router;
