df78c04db706369e0da7c264cd41b4b0eb9a75b1
Michael Durfey <mdurfey@me.com>
2020-07-21 22:21:47 -0700

    deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use("/", routes);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   // eslint-disable-next-line no-console
-  console.log("listening on port 3000!");
+  console.log("listening on port" + process.env.PORT || 5000);
 });
